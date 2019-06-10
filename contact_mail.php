a4a9e23cafac049d11031264325323b4e50ea27a
Dheenadhayalan <AmigoDheena@users.noreply.github.com>
2019-06-10 16:18:12 +0530

    Create contact_mail.php

---
+++ contact_mail.php
@@ -0,0 +1,9 @@
+<?php
+$toEmail = "dheena.askan@gmail.com"; // Test mail
+$mailHeaders = "From: " . $_POST["userName_page"] . "<". $_POST["userEmail_page"] .">\r\n";
+if(mail($toEmail, $_POST["phone_page"], $_POST["content_page"], $mailHeaders)) {
+print "<p class='success'>Contact Mail Sent.</p>";
+} else {
+print "<p class='Error'>Problem in Sending Mail.</p>";
+}
+?>
